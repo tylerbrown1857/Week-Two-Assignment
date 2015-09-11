5033889dd87b99ae47f4395c8d0f197e8da7f89a
Daniel Neumann <dkneuma@gmail.com>
2015-09-11 15:08:46 -0400

    Added default program

---
+++ default.py
@@ -0,0 +1,13 @@
+# Default shell for a Python 3.x program
+# Copy this and rename it to write your own code
+#
+__author__ = 'Your Name'
+
+# Your class
+# Name of program
+#
+# Brief description of what program does.
+
+X = 2
+name = "bob"
+print(X * name)
