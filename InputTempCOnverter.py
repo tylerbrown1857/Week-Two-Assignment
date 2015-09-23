eace9118b46346c31140cfb3d29f38a650cffc4e
tylerbrown1857 <tyler.brown1857@myemail.indwes.edu>
2015-09-23 03:05:19 +0000

    done

---
+++ InputTempCOnverter.py
@@ -0,0 +1,12 @@
+__author__ = 'Tyler Brown'
+def main():
+    x = 0
+    while x = 0:
+        f = eval(input("Enter a Temperature in Fahrenheit:"))
+        c = (f-32)/1.8
+        print(f, " degrees in Fahrenheit is equal to ", c, " degrees in Celcius")
+        
+        
+main()
+        
+        
